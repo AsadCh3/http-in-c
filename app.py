caf9761b970a74044f6fe3e9793e2b4ffe4a116a
Asad Ali <102943014+AsadCh3@users.noreply.github.com>
2023-09-30 01:50:23 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -9,24 +9,24 @@ orders = [
 		"customer_name": "John Flask"
 	},
 	{
-		"order_id": 1,
-		"product_name": "Fruitamin Orange",
-		"customer_name": "John Flask"
+		"order_id": 2,
+		"product_name": "Nestle Milkpack",
+		"customer_name": "Rais M"
 	},
 	{
-		"order_id": 1,
-		"product_name": "Fruitamin Orange",
-		"customer_name": "John Flask"
+		"order_id": 3,
+		"product_name": "Nestle Milo",
+		"customer_name": "Joseph W"
 	},
 	{
-		"order_id": 1,
-		"product_name": "Fruitamin Orange",
-		"customer_name": "John Flask"
+		"order_id": 4,
+		"product_name": "RedBull Drink",
+		"customer_name": "Albert C"
 	},
 	{
-		"order_id": 1,
-		"product_name": "Fruitamin Orange",
-		"customer_name": "John Flask"
+		"order_id": 2,
+		"product_name": "Fruitamin Mango",
+		"customer_name": "Martin D"
 	}
 ]
 
